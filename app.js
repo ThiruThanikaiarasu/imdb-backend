82f027927f85e8cb5ae53f90194afff36c048a48
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-05-31 21:28:36 +0530

    Add. Included router and DB connection established.

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 3500
+const moviesList = require('./routers/moviesList')
 
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on("error",(errorMessage)=> console.log(errorMessage))
+db.once("open", ()=> console.log("Connection Established"))
+
+
+app.get('/',(request,response)=>{
+    response.status(200).send('This is App.js')
+})
+
+app.use('/api/v1/movieList',moviesList)
 
 app.listen(PORT,console.log(`Server is running at http://localhost:${PORT}`))
