01d4fc2ec9ce2af321a327c54c480adbec8a16f1
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-02 23:21:07 +0530

    Fix. Set the fields name, enrolledDepartment as required.

---
+++ models/students.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const studentSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    enrollDepartment : {
+        type : String,
+        required : true,
+    },
+    enrollmentDate : {
+        type : Date,
+        default : Date.now(),
+    },
+})
+
+
+
+module.exports = mongoose.model('studentModel',studentSchema)
+
--- routers/students.js
+++ routers/students.js
@@ -1,23 +1,83 @@
 const express = require('express')
 const router = express.Router()
+const studentModel = require('../models/students')
 
-router.get('/',(request,response)=>{
-    response.send("Display all available details")
-})
-router.post('/',(request,response)=>{
-    response.send("Add a new detail")
-})
-router.get('/:id',(request,response)=>{
-    response.send(`Display a detail with id ${request.params.id}`)
-})
-router.patch('/:id',(request,response)=>{
-    response.send(`Update a detail with id ${request.params.id}`)
-})
-router.delete('/:id',(request,response)=>{
-    response.send(`Display a detail with id ${request.params.id}`)
+// db is external dependency so we use async
+router.get('/', async (request,response)=>{
+    const students = await studentModel.find();
+    try{
+        response.status(200).json(students)
+    }
+    catch(error){
+        response.send(500).json({message : error.message})
+    }
 })
 
 
+router.post('/', async (request,response)=>{
+    const newStudent = new studentModel({
+        name : request.body.name,
+        enrollDepartment :request.body.enrollDepartment,
+        enrollmentDate : request.body.enrollmentDate
+    })
+    try{
+        const student = await newStudent.save()
+        response.json(student)
+    }
+    catch(error)
+    {
+        response.json({errorMessage : error.message})
+    }
+    
+})
+
+router.get('/:id',getStudent, (request,response)=>{
+    response.status(200).json(response.student)
+})
+router.patch('/:id',getStudent, async(request,response)=>{
+    if(request.body.name != null)
+    {
+        response.student.name = request.body.name
+    }
+
+    if(request.body.enrollDepartment != null)
+    {
+        response.student.enrollDepartment = request.body.enrollDepartment
+    }
+
+    try{
+        const updatedStudent = await response.student.save()
+        response.status(200).json(updatedStudent)
+    }
+    catch(error){
+        response.status(500).json({message : error.message})
+    }
+})
+router.delete('/:id',getStudent, async(request,response)=>{
+    try{
+        await response.student.deleteOne();
+        response.json({message : `Deleted the user ${response.student.name}`}) // why not status ???
+    }
+    catch(error){
+        response.status(500).json({message : error.message})
+    }
+})
+
+async function getStudent(request,response,next){
+    let student
+    try{
+        student = await studentModel.findById(request.params.id)
+        if(student == null)
+        {
+            return response.status(500).json({message: error.message})
+        }
+    }
+    catch(error){
+        return response.status(500).json({message : error.message})
+    }
+    response.student = student
+    next()
+}
 
 
 module.exports = router
